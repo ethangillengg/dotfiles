f8bf6f32ccb7a6ed4e256dc9325f0c37fc07a75b
Ethan Gill <ethan.gill@ucalgary.ca>
2023-03-10 09:08:27 -0700

    feat: feh + manga reader alias
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

---
+++ home/ethan/features/desktop/feh.nix
@@ -0,0 +1,83 @@
+{
+  programs.feh = {
+    enable = true;
+    keybindings = {
+      menu_parent = [
+        "h"
+        "Left"
+      ];
+      menu_child = [
+        "l"
+        "Right"
+      ];
+      menu_down = [
+        "j"
+        "Down"
+      ];
+      menu_up = [
+        "k"
+        "Up"
+      ];
+      menu_select = [
+        "space"
+        "Return"
+      ];
+
+      # Same for image navigation ...
+      next_img = [
+        "C-j"
+        "n"
+        "space"
+      ];
+      prev_img = [
+        "C-k"
+        "p"
+        "BackSpace"
+      ];
+
+      # and image movement
+      scroll_up = [
+        "k"
+        "Up"
+        "s"
+      ];
+      scroll_down = [
+        "j"
+        "Down"
+        "w"
+      ];
+      scroll_left = [
+        "h"
+        "Left"
+        "d"
+      ];
+      scroll_right = [
+        "l"
+        "Right"
+        "a"
+      ];
+      # zooming
+      zoom_in = [
+        "W"
+        "C-Up"
+        "K"
+      ];
+
+      zoom_out = [
+        "S"
+        "J"
+        "C-Down"
+      ];
+      toggle_aliasing = "r";
+      # I only hit these accidentally
+      save_image = [ ];
+      save_filelist = [ ];
+    };
+    buttons = {
+      zoom_in = [ 4 1 ];
+      zoom_out = 5;
+      next_img = 3;
+
+    };
+  };
+}
