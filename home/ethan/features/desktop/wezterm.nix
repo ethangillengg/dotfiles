d6effafda068038f2e1abe2dce8e3e90e3631301
Ethan Gill <ethan.gill@ucalgary.ca>
2023-04-01 20:55:25 -0600

    fix: wezterm

--- home/ethan/features/desktop/wezterm.nix
+++ home/ethan/features/desktop/wezterm.nix
@@ -4,6 +4,11 @@
     extraConfig = ''
       return {
         color_scheme = "Ayu Mirage",
+        hide_cursor_when_typing = false,
+        enable_tab_bar = false,
+        font_size = 12.0,
+        initial_cols = 120,
+        initial_rows = 32
       }
     '';
   };
