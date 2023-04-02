1e5a8c22d2256f94cab774ed01e1b6070eaae0b8
Ethan Gill <ethan.gill@ucalgary.ca>
2023-04-01 23:43:21 -0600

    feat: lf bulk rename
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/cli/lf/default.nix
+++ home/ethan/features/cli/lf/default.nix
@@ -18,6 +18,31 @@
       #       esac
       #   }}
       # '';
+      "bulk-rename" = ''
+        ''${{
+            old="$(mktemp)"
+            new="$(mktemp)"
+            if [ -n "$fs" ]; then
+                fs="$(basename -a $fs)"
+            else
+                fs="$(ls)"
+            fi
+            printf '%s\n' "$fs" >"$old"
+            printf '%s\n' "$fs" >"$new"
+            nvim "$new"
+            [ "$(wc -l < "$new")" -ne "$(wc -l < "$old")" ] && exit
+            paste "$old" "$new" | while IFS= read -r names; do
+                src="$(printf '%s' "$names" | cut -f1)"
+                dst="$(printf '%s' "$names" | cut -f2)"
+                if [ "$src" = "$dst" ] || [ -e "$dst" ]; then
+                    continue
+                fi
+                mv -- "$src" "$dst"
+            done
+            rm -- "$old" "$new"
+            lf -remote "send $id unselect"
+        }}
+      '';
 
       "trash" = "%trash put $fx";
     };
@@ -33,6 +58,7 @@
       "H" = ":set hidden!";
 
       "<delete>" = "trash";
+      "R" = ":bulk-rename";
     };
 
     extraConfig = ''
