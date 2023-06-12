c6144dc35b6be88a8d2a40dc654eebc89d39261c
Ethan Gill <ethan.gill@ucalgary.ca>
2023-06-12 16:06:24 -0600

    feat: faster keyboard repeat
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/desktop/wayland-wm/hyprland/default.nix
+++ home/ethan/features/desktop/wayland-wm/hyprland/default.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  lib,
   config,
   pkgs,
   ...
@@ -71,6 +70,11 @@ in {
           # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
           pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
           preserve_split = yes # you probably want this
+      }
+
+      input {
+          repeat_rate = 35
+          repeat_delay = 250
       }
 
 
