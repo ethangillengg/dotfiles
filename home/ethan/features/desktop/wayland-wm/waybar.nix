af5ae30f29018bade5155b88f8dfc25bcc7a9072
Ethan Gill <ethan.gill@ucalgary.ca>
2023-06-11 23:10:30 -0600

    feat: fix ethernet icon
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/desktop/wayland-wm/waybar.nix
+++ home/ethan/features/desktop/wayland-wm/waybar.nix
@@ -207,7 +207,7 @@ in {
         network = {
           interval = 3;
           format-wifi = " {essid}";
-          format-ethernet = " Connected";
+          format-ethernet = "󰈀 Connected";
           format-disconnected = "";
           tooltip-format = ''
             {ifname}
