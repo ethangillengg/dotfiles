12040108050545fd9674cbfbd66e068eb89cbb7d
Ethan Gill <ethan.gill@ucalgary.ca>
2023-06-14 17:11:23 -0600

    feat: use kitty for sixel image preview in lf

--- home/ethan/features/cli/lf/preview.sh
+++ home/ethan/features/cli/lf/preview.sh
@@ -4,13 +4,10 @@ w=$2
 h=$3
 x=$4
 y=$5
-hfloat=$(expr $h*0.9 | bc)
-hint=${hfloat/.*}
-CACHE="$HOME/.cache/tmpimg"
 
 if [[ "$( file -Lb --mime-type "$file")" =~ ^image ]]; then
-  kitty +icat --silent --transfer-mode file --place "${w}x${h}@${x}x2" "$file"
-  exit 1
+    kitty +kitten icat --silent --stdin no --transfer-mode file --place "${w}x${h}@${x}x${y}" "$file" < /dev/null > /dev/tty
+    exit 1
 fi
 
-ctpv "$file"
+pistol "$file"
