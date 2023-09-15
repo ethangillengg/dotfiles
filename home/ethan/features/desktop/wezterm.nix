3ed98cc8b55010801519adf786ffd0f0d619f30d
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-15 14:33:56 -0600

    feat: catppuccin

--- home/ethan/features/desktop/wezterm.nix
+++ home/ethan/features/desktop/wezterm.nix
@@ -1,7 +1,7 @@
 {config, ...}: let
   # inherit (config) colorscheme;
   # inherit (colorscheme) colors;
-  # color_scheme = "Catppuccin Mocha",
+  # color_scheme = "Gruvbox dark, medium (base16)",
   # color_scheme = "Material Darker (base16)",
 in {
   programs.wezterm = {
@@ -9,7 +9,7 @@ in {
     extraConfig = ''
       return {
         font_size = 14.0,
-        color_scheme = "Gruvbox dark, medium (base16)",
+        color_scheme = "Catppuccin Mocha",
         font = wezterm.font("${config.fontProfiles.monospace.family}"),
         hide_tab_bar_if_only_one_tab = true,
         enable_tab_bar = false,
--- home/ethan/thinkpad.nix
+++ home/ethan/thinkpad.nix
@@ -14,14 +14,15 @@
   #Favs:
   # colorscheme = inputs.nix-colors.colorschemes.material-darker;
   # colorscheme = inputs.nix-colors.colorschemes.catppuccin-macchiato;
-  # colorscheme = inputs.nix-colors.colorschemes.catppuccin-mocha;
+  colorscheme = inputs.nix-colors.colorschemes.catppuccin-mocha;
   # colorscheme = inputs.nix-colors.colorschemes.material-palenight;
   # colorscheme = inputs.nix-colors.colorschemes.nord;
   # colorscheme = inputs.nix-colors.colorschemes.rose-pine;
   # colorscheme = inputs.nix-colors.colorschemes.rose-pine-moon;
+  # colorscheme = inputs.nix-colors.colorschemes.gruvbox-material-dark-hard;
 
-  colorscheme = inputs.nix-colors.colorschemes.gruvbox-material-dark-hard;
-  wallpaper = outputs.wallpapers.gruvbox-pacman;
+  # wallpaper = outputs.wallpapers.gruvbox-pacman;
+  wallpaper = outputs.wallpapers.catppuccin-buttons;
 
   # monitors = [{
   #   name = "eDP-1";
