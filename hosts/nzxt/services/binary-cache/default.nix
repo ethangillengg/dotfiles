fc224f135cd29338123a1e64715cbdfdf46fe26e
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-16 11:51:31 -0600

    feat: enable nginx nix-cache
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/nzxt/services/binary-cache/default.nix
+++ hosts/nzxt/services/binary-cache/default.nix
@@ -15,15 +15,15 @@
       package = pkgs.nix-serve.override {nix = pkgs.nixVersions.nix_2_12;};
       port = 9999;
     };
-    # nginx.virtualHosts."cache.m7.rs" = {
-    #   forceSSL = true;
-    #   enableACME = true;
-    #   locations."/".extraConfig = ''
-    #     proxy_pass http://localhost:${toString config.services.nix-serve.port};
-    #     proxy_set_header Host $host;
-    #     proxy_set_header X-Real-IP $remote_addr;
-    #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    #   '';
-    # };
+    nginx.virtualHosts."nzxt.emerald-duck.ts.net" = {
+      # forceSSL = true;
+      # enableACME = true;
+      locations."/".extraConfig = ''
+        proxy_pass http://localhost:${toString config.services.nix-serve.port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
+    };
   };
 }
