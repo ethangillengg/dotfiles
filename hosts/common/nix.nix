fdccaa3a04e350dc7240945b3e6e050569c7ce94
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-16 09:22:23 -0600

    feat: use community binary cache
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -1,5 +1,38 @@
 {
-  nix.settings.auto-optimise-store = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  lib,
+  inputs,
+  ...
+}: {
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.m7.rs"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      warn-dirty = false;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 3 generations
+      options = "--delete-older-than +3";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+    # Add nixpkgs input to NIX_PATH
+    # This lets nix2 commands still use <nixpkgs>
+    nixPath = ["nixpkgs=${inputs.nixpkgs.outPath}"];
+  };
   nixpkgs.config.allowUnfree = true;
 }
