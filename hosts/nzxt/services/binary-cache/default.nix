e6be8558e7531db5595266f02c4a37b445cb7c43
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-16 12:18:34 -0600

    fix(WIP): try fix nix-cache key

--- hosts/nzxt/services/binary-cache/default.nix
+++ hosts/nzxt/services/binary-cache/default.nix
@@ -13,7 +13,7 @@
       secretKeyFile = config.sops.secrets.cache-sig-key.path;
       # TODO: temporary fix for NixOS/nix#7704
       package = pkgs.nix-serve.override {nix = pkgs.nixVersions.nix_2_12;};
-      port = 9999;
+      # port = 9999;
     };
     nginx.virtualHosts."nzxt.emerald-duck.ts.net" = {
       # forceSSL = true;
