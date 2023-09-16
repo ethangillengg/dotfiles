a442aec3d506a092f0b13be07af036e9baa4cb8d
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-16 12:18:34 -0600

    fix(WIP): try fix nix-cache key
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -8,13 +8,15 @@
       substituters = [
         # "https://nix-community.cachix.org"
         # "https://cache.nixos.org/"
-        "http://nzxt.emerald-duck.ts.net:9999"
+        # "http://nzxt.emerald-duck.ts.net:9999"
       ];
       trusted-public-keys = [
         # "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="
         # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "nzxt.emerald-duck.ts.net:9999:7+2mxpoaF+NGTHZ7q2SnHxmmAfzc63pS41Scjxyz+gE="
-        "nzxt.emerald-duck.ts.net:7+2mxpoaF+NGTHZ7q2SnHxmmAfzc63pS41Scjxyz+gE="
+        # "nzxt.emerald-duck.ts.net:7+2mxpoaF+NGTHZ7q2SnHxmmAfzc63pS41Scjxyz+gE="
+
+        # nzxt.emerald-duck.ts.net:VLHUz+n+brCRI57Dk45pmjMLAN2Iznpyxh2vVVrRaSc=
       ];
 
       auto-optimise-store = lib.mkDefault true;
