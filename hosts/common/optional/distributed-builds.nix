c6e27857f270401fe5d0293ae662078919ea4d08
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-19 12:52:14 -0600

    fix: lower speed nzxt distributed build

--- hosts/common/optional/distributed-builds.nix
+++ hosts/common/optional/distributed-builds.nix
@@ -12,20 +12,9 @@
         # replace the previous line by, e.g.,
         # systems = ["x86_64-linux" "aarch64-linux"];
         maxJobs = 8;
-        speedFactor = 1;
+        speedFactor = 2;
         supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
         mandatoryFeatures = [];
-      }
-      {
-        hostName = "localhost";
-        system = "x86_64-linux";
-        maxJobs = 4;
-        # This is the only builder marked as aarch64, so these builds will always
-        # run here (regardless of speedFactor).
-        # As for x86_64, this machine's factor is much lower than the others, so
-        # these builds will only be picked up if the others are offline.
-        speedFactor = 1;
-        supportedFeatures = ["kvm" "big-parallel" "nixos-test"];
       }
     ];
     distributedBuilds = true;
