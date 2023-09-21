cdb122296280b7d76fc4f1e9046a2746bf349fae
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-21 14:41:05 -0600

    feat: migrate to wpa_supplicant + eduroam
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

---
+++ hosts/common/optional/wireless.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  environment.systemPackages = [pkgs.wpa_supplicant_gui];
+  # Wireless secrets stored through sops
+  sops.secrets.wireless = {
+    sopsFile = ../secrets.yaml;
+    neededForUsers = true;
+  };
+
+  networking.wireless = {
+    enable = true;
+    fallbackToWPA2 = false;
+    # Declarative
+    environmentFile = config.sops.secrets.wireless.path;
+    networks = {
+      "eduroam" = {
+        auth = ''
+          key_mgmt=WPA-EAP
+          pairwise=CCMP
+          auth_alg=OPEN
+          eap=PEAP
+          identity="ethan.gill"
+          password="@EDUROAM@"
+          phase2="auth=MSCHAPV2"
+        '';
+      };
+    };
+
+    # Imperative
+    allowAuxiliaryImperativeNetworks = true;
+    userControlled = {
+      enable = true;
+      group = "network";
+    };
+    extraConfig = ''
+      update_config=1
+    '';
+  };
+
+  # Ensure group exists
+  users.groups.network = {};
+
+  systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
+}
