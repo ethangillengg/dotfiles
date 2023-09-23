69619428aed274e73ba2fcf36007aad00db2cb8c
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-23 14:21:58 -0600

    feat: add git signing

--- home/ethan/features/cli/git.nix
+++ home/ethan/features/cli/git.nix
@@ -3,6 +3,10 @@
     enable = true;
     userName = "Ethan Gill";
     userEmail = "ethan.gill@ucalgary.ca";
+    signing = {
+      signByDefault = true;
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
