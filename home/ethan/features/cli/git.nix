1d8ea5eb99b06780c5dc01f35366f76581d3c769
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-25 12:35:09 -0600

    feat: delta git diff
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/cli/git.nix
+++ home/ethan/features/cli/git.nix
@@ -7,6 +7,13 @@
       signByDefault = true;
       key = "4AF98CB0736E4B95DEE74A28BC3D2A808673D24F";
     };
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = true;
+      };
+    };
 
     aliases = {
       graph = "log --decorate --oneline --graph";
