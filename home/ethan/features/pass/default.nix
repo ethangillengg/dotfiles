5cbcb5c7d6f8b88d0fd67bef57ccdb210842e431
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-24 20:26:40 -0600

    fix: browserpass extension
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/pass/default.nix
+++ home/ethan/features/pass/default.nix
@@ -1,19 +1,10 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   programs.password-store = {
     enable = true;
     settings = {PASSWORD_STORE_DIR = "$HOME/.password-store";};
     package = pkgs.pass.withExtensions (p: [p.pass-otp]);
   };
 
-  programs.firefox.package = with pkgs; (firefox.override {extraNativeMessagingHosts = [browserpass];});
-
-  # services.pass-secret-service = {
-  #   enable = true;
-  #   storePath = "${config.home.homeDirectory}/.password-store";
-  #   extraArgs = ["-e${config.programs.password-store.package}/bin/pass"];
-  # };
+  # firefox extension
+  programs.browserpass.enable = true;
 }
