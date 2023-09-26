df5bea6fde1ecb72a061b2f3dccb33640f24a859
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-26 13:42:48 -0600

    feat: better fish greeting (w/ quote)
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- pkgs/quote/quote.sh
+++ pkgs/quote/quote.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-curl -s https://api.quotable.io/random?maxLength=50 | jq -r '{content, author} | "\"\(.content)\"\n\n- \(.author)"'
+
+quote=$(curl -s https://api.quotable.io/random?maxLength=50)
+
+if [ -z "$quote" ]; then
+  printf "\"404 quote not found\"\n- Me\n"
+else
+  echo $quote | jq -r '{content, author} | "\"\(.content)\"\n- \(.author)"'
+fi
+
+
