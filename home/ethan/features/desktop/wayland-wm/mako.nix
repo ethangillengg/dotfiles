9dbcb76ce8e021cf6f95497ba2fd0afc8385d905
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-26 13:43:37 -0600

    feat: taller notifications
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/desktop/wayland-wm/mako.nix
+++ home/ethan/features/desktop/wayland-wm/mako.nix
@@ -10,10 +10,10 @@ in {
     font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
     anchor = "top-right";
-    width = 400;
-    height = 150;
+    width = 450;
+    height = 200;
     borderSize = 2;
-    defaultTimeout = 5000;
+    defaultTimeout = 8000;
     backgroundColor = "#${colors.base00}dd";
     borderColor = "#${colors.base03}dd";
     textColor = "#${colors.base05}dd";
