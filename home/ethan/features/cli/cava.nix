cd583b7a35b4c659ccec536e9295714d1b916664
Ethan Gill <ethan.gill@ucalgary.ca>
2023-10-14 13:03:10 -0600

    feat: cava themed gradient
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

---
+++ home/ethan/features/cli/cava.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (config.colorscheme) colors;
+in {
+  home.packages = [pkgs.cava];
+  home.file.".config/cava/config".text = ''
+    [general]
+    bar_width = 1
+    bar_spacing = 0
+    [color]
+
+    # ncurses output method and a terminal that can change color definitions such as Gnome-terminal or rxvt.
+    # if supported, ncurses mode will be forced on if user defined colors are used.
+    # default is to keep current terminal color
+    ;background = default
+    ;foreground = default
+    # SDL only support hex code colors, these are the default:
+    ;background = '#111111'
+    ; foreground = '#33cccc'
+    # Gradient mode, only hex defined colors (and thereby ncurses mode) are supported,
+    # background must also be defined in hex  or remain commented out. 1 = on, 0 = off.
+    # You can define as many as 8 different colors. They range from bottom to top of screen
+    gradient = 1
+    gradient_count = 3
+    gradient_color_1 = '#${colors.base0C}'
+    gradient_color_2 = '#${colors.base05}'
+    gradient_color_3 = '#${colors.base05}'
+  '';
+}
