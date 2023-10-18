d98601b9f5ea974d26e738d2ca1b98e4e347b12c
Ethan Gill <ethan.gill@ucalgary.ca>
2023-10-18 10:33:28 -0600

    feat: screenshots on sway

--- hosts/thinkpad/hardware-configuration.nix
+++ hosts/thinkpad/hardware-configuration.nix
@@ -88,7 +88,6 @@
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 
-  # networking.enableIPv6 = false;
   services.throttled.enable = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   # powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
