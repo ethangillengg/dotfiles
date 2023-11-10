97b681abe6a3055c8e4efd2d2e82fe972be1ec95
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-10 09:25:49 -0700

    feat: lofi script

--- home/ethan/features/cli/aliases.nix
+++ home/ethan/features/cli/aliases.nix
@@ -26,6 +26,9 @@
 
     lsblk = "lsblk -o NAME,FSUSED,FSSIZE,FSUSE%";
 
+    # Play lofi-girl
+    lofi = "mpv --player-operation-mode=cplayer --speed=1.0 --ytdl-format=\"bestaudio\" \"ytdl://youtube.com/watch?v=jfKfPfyJRdk\"";
+
     # hmc = "nvim \"+Man home-configuration.nix | only\"";
     hmc = "man home-configuration.nix";
     hmp = "home-manager packages | fzf";
