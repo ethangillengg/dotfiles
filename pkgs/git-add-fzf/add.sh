ede847eab56bfbac70d52f7f33c3252f251cbee8
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-11 22:16:10 -0700

    chore: remove unused
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- pkgs/git-add-fzf/add.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-selected=$(git -c color.status=always -c status.relativePaths=true status -su | fzf --height 80% --preview 'git diff HEAD --color=always {2} | delta' --ansi -m -0)
-git add $(printf "$selected" | nawk 'NF=2{print $2}')
-
-printf "\n\033[1mNew Git Status:\033[0m\n"
-git status --short
-
