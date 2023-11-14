59ed94a4e8573332e8af73e9c419aa5ebfa81bb5
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-14 16:50:08 -0700

    chore: reorganize applications

---
+++ home/ethan/features/desktop/common/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./qt.nix
+    ./gtk.nix
+  ];
+}
--- home/ethan/features/desktop/default.nix
+++ home/ethan/features/desktop/default.nix
@@ -1,14 +1,7 @@
-{pkgs, ...}: {
+{
   imports = [
-    ./rio.nix
-    ./wezterm.nix
-    ./feh.nix
-    ./rofi.nix
     ./wayland-wm
-    ./mpv.nix
-    ./wpa-gui.nix
-
-    ./common/qt.nix
-    ./common/gtk.nix
+    ./applications
+    ./common
   ];
 }
