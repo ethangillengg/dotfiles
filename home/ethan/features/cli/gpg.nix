2126d484e49caf357b69b9bee6c1527ba20c609b
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-14 10:20:32 -0700

    feat: gpg cache 30 min

--- home/ethan/features/cli/gpg.nix
+++ home/ethan/features/cli/gpg.nix
@@ -3,12 +3,15 @@
     packages = [pkgs.pinentry-gnome pkgs.gcr];
     name = "gnome3";
   };
+
+  minsToSecs = mins: (mins * 60);
 in {
   home.packages = pinentry.packages;
 
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    defaultCacheTtl = minsToSecs 30;
     sshKeys = [""];
     pinentryFlavor = pinentry.name;
     enableExtraSocket = true;
