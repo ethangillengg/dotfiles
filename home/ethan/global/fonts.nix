ff6a4b222504200d1e8488610b3936a6e3bb22aa
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-19 13:44:51 -0700

    feat: better jp fonts
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/global/fonts.nix
+++ home/ethan/global/fonts.nix
@@ -1,28 +1,26 @@
 {pkgs, ...}: {
   fontProfiles = {
     enable = true;
-    # monospace = {
-    #   family = "FiraCode Nerd Font";
-    #   package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
-    # };
-    # regular = {
-    #   family = "Fira Sans";
-    #   package = pkgs.fira;
-    # };
 
     monospace = {
       family = "JetBrainsMono Nerd Font";
-      # family = "Fira Code Nerd Font";
       package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono" "FiraCode"];};
     };
+
     regular = {
       family = "JetBrainsMono Nerd Font";
-      # family = "Fira Code Nerd Font";
       package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono" "FiraCode"];};
     };
+
     fallback = {
-      family = "Noto Sans CJK JP";
-      package = pkgs.noto-fonts-cjk;
+      family = "Rounded Mgen+ 1c";
+      package = pkgs.rounded-mgenplus;
     };
   };
+
+  # Extra fonts
+  home.packages = with pkgs; [
+    fontpreview # easy font preview
+    rictydiminished-with-firacode
+  ];
 }
