c8238ddf9aed5c4c70749de6a043d44a460bd7e7
Ethan Gill <ethan.gill@ucalgary.ca>
2024-01-18 07:47:33 -0700

    feat(nfs): automount
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/thinkpad/network-fs.nix
+++ hosts/thinkpad/network-fs.nix
@@ -8,60 +8,104 @@
     "noauto"
     "noatime"
     "x-systemd.idle-timeout=600" # disconnects after 10 minutes idle (i.e. 600 seconds)
-  ];
-
-  sambaMountOptions = [
-    "x-systemd.automount"
-    "noauto"
-    "x-systemd.idle-timeout=600"
-    "x-systemd.device-timeout=2s"
-    "x-systemd.mount-timeout=5s"
-    "vers=3.0" # Use the highest protocol version supported by both client and server for better performance
-    "credentials=${config.sops.secrets.samba-credentials.path}"
-    "uid=1000" # Replace 1000 with the actual user ID
-    "gid=100" # Replace 100 with the actual group ID
-    "file_mode=0775" # Modify as needed based on your permissions requirements
-    "dir_mode=0775" # Modify as needed based on your permissions requirements
-    "cache=strict" # Use 'strict' or 'none' depending on needs (default is 'loose')
-    "rsize=1048576" # Read buffer size (1MB); increase if you have large reads
-    "wsize=1048576" # Write buffer size (1MB); increase if you have large writes
-    "actimeo=1" # The attribute cache timeout in seconds (shorter might lead to more requests but ensures freshness)
-    "nobrl" # Disable byte-range locking if not necessary
+    "x-systemd.requires=tailscale-online.target" # requires tailscale daemon to be running
   ];
   mediaServer = "100.113.35.34";
 in {
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = with pkgs; [cifs-utils nfs-utils];
-
-  # SAMBA
-  fileSystems = {
-    "/mnt/samba/data" = {
-      device = "//${mediaServer}/data";
-      fsType = "cifs";
-      options = sambaMountOptions;
-    };
-    "/mnt/samba/media" = {
-      device = "//${mediaServer}/media";
-      fsType = "cifs";
-      options = sambaMountOptions;
-    };
-  };
+  services.rpcbind.enable = true; # needed for NFS
 
   # NFS
-  fileSystems = {
-    "/mnt/nfs/data" = {
-      device = "${mediaServer}:/data";
-      fsType = "nfs";
-      options = nfsMountOptions;
+  # fileSystems = {
+  #   # "/mnt/nfs/data" = {
+  #   #   device = "${mediaServer}:/data";
+  #   #   fsType = "nfs";
+  #   #   options = nfsMountOptions;
+  #   # };
+  #   "/mnt/nfs/media" = {
+  #     device = "${mediaServer}:/media";
+  #     fsType = "nfs";
+  #     options = nfsMountOptions;
+  #   };
+  # };
+
+  systemd.mounts = let
+    commonMountOptions = {
+      type = "nfs";
+      mountConfig = {
+        Options = "noatime";
+      };
     };
-    "/mnt/nfs/media" = {
-      device = "${mediaServer}:/media";
-      fsType = "nfs";
-      options = nfsMountOptions;
+  in [
+    (commonMountOptions
+      // {
+        what = "${mediaServer}:/media";
+        where = "/mnt/nfs/media";
+      })
+  ];
+
+  systemd.automounts = let
+    commonAutoMountOptions = {
+      wantedBy = ["multi-user.target"];
+      # requires = ["tailscaled.service"];
+      after = ["tailscaled.service"];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+    };
+  in [
+    (commonAutoMountOptions // {where = "/mnt/nfs/media";})
+  ];
+
+  # "tailscale-online.target" to only mount nfs shares when tailnet is up
+  # see: https://forum.tailscale.com/t/mount-share-only-if-connected-to-tailscale/3027/7
+  systemd.timers."tailscale-dispatcher" = {
+    bindsTo = ["tailscaled.service"];
+    after = ["tailscaled.service"];
+    wantedBy = ["tailscaled.service"];
+    timerConfig = {
+      OnBootSec = "0";
+      OnUnitInactiveSec = "10";
+      AccuracySec = "1";
     };
   };
-
-  sops.secrets.samba-credentials = {
-    sopsFile = ../common/secrets.yaml;
+  systemd.services."tailscale-dispatcher" = {
+    requisite = ["tailscaled.service"];
+    after = ["tailscaled.service"];
+    serviceConfig.Type = "oneshot";
+    script = with pkgs; ''
+      get-state() {
+        if ${systemd}/bin/systemctl is-active --quiet tailscaled.service && [[ $(${tailscale}/bin/tailscale status --peers=false --json=true | ${jq}/bin/jq -r '.Self.Online') = "true" ]]; then
+          current_state="online"
+        else
+          current_state="offline"
+        fi
+      }
+      transition() {
+        if [[ "$current_state" != "$prev_state" ]]; then
+          if [[ "$current_state" = "online" ]]; then
+            ${systemd}/bin/systemctl start tailscale-online.target
+          else
+            ${systemd}/bin/systemctl stop tailscale-online.target
+          fi
+          echo "$current_state" > /tmp/tailscale-state
+        fi
+      }
+      check-state() {
+        get-state
+        if [[ -s /tmp/tailscale-state ]]; then
+          prev_state=$(</tmp/tailscale-state)
+        else
+          prev_state="offline"
+        fi
+        transition
+      }
+      check-state
+    '';
+  };
+  systemd.targets."tailscale-online" = {
+    after = ["tailscaled.service"];
+    bindsTo = ["tailscaled.service"];
   };
 }
