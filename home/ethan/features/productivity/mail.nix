ec9029fae014a2603fae848ccf80d9c808ffe992
Ethan Gill <ethan.gill@ucalgary.ca>
2024-01-22 12:59:59 -0700

    fix(thunderbird): auth with ucalgary

--- home/ethan/features/productivity/mail.nix
+++ home/ethan/features/productivity/mail.nix
@@ -40,6 +40,27 @@ in {
           perIdentitySettings = id: {
             "mail.identity.id_${id}.protectSubject" = false;
           };
+
+          settings = id: {
+            # Folders
+            "mail.identity.id_${id}.draft_folder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Drafts";
+            "mail.identity.id_${id}.fcc_folder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Sent%20Items";
+            "mail.identity.id_${id}.stationery_folder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Templates";
+            "mail.identity.id_${id}.archive_folder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Archive";
+            "mail.identity.id_${id}.junk_folder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Junk%20Email";
+            "mail.server.server_${id}.trash_folder_name" = "Deleted%20Items";
+            "mail.server.server_${id}.spamActionTargetFolder" = "imap://ethan.gill%40ucalgary.ca@outlook.office365.com/Junk%20Email";
+
+            ## Authentication needs the OAuth params:
+            # IMAP
+            "mail.server.server_${id}.authMethod" = 10; # OAuth2
+            "mail.server.server_${id}.oauth2.issuer" = "login.microsoftonline.com";
+            "mail.server.server_${id}.oauth2.scope" = "https://outlook.office.com/IMAP.AccessAsUser.All https://outlook.office.com/POP.AccessAsUser.All https://outlook.office.com/SMTP.Send offline_access";
+            # SMTP
+            "mail.smtpserver.smtp_${id}.authMethod" = 10; # OAuth2
+            "mail.smtpserver.smtp_${id}.oauth2.issuer" = "login.microsoftonline.com";
+            "mail.smtpserver.smtp_${id}.oauth2.scope" = "https://outlook.office.com/IMAP.AccessAsUser.All https://outlook.office.com/POP.AccessAsUser.All https://outlook.office.com/SMTP.Send offline_access";
+          };
         };
 
         imap = {
