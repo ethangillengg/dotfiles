5d6158b3c2674d830685d2cf3c86e8ea2fbab9ba
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-24 02:50:08 -0700

    feat(nzxt): nvidia
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/nzxt/hardware-configuration.nix
+++ hosts/nzxt/hardware-configuration.nix
@@ -12,6 +12,28 @@
   ];
   services.xserver.videoDrivers = ["nvidia"];
   hardware.opengl.enable = true;
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+  };
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
