932f6fb83434e9d61a1446da553589f99a851939
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-24 02:28:20 -0700

    feat: `mods` and `aichat`

--- home/ethan/features/cli/chatgpt.nix
+++ home/ethan/features/cli/chatgpt.nix
@@ -1,11 +1,36 @@
 {pkgs, ...}: let
   pass = "${pkgs.pass}/bin/pass";
+
+  formats = with pkgs.formats; {
+    yaml = yaml {};
+  };
 in {
   home.shellAliases = {
-    gpt = "OPENAI_API_KEY=$(${pass} show personal/openai) chatgpt";
+    ai = "OPENAI_API_KEY=$(${pass} show personal/openai) aichat";
   };
 
-  home.packages = [
-    pkgs.chatgpt-cli
+  xdg.configFile = {
+    "aichat/config.yaml".source = formats.yaml.generate "aichat-config" {
+      keybindings = "vi";
+      model = "openai:gpt-4-turbo-preview";
+      wrap = 120;
+    };
+
+    "aichat/roles.yaml".source = formats.yaml.generate "aichat-roles" [
+      {
+        name = "shell";
+        temperature = 0.7;
+        prompt = "Act as a linux shell expert. Answer only with code, do not write explanations.";
+      }
+      {
+        name = "nix";
+        prompt = "Act as an expert in Nix, NixOS, home-manager, and Neovim. Solve problems, and provide examples in a clear and helpful manner.";
+      }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    aichat
+    mods
   ];
 }
