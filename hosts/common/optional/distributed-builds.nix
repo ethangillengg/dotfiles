7c3b152e8deb8c08faf3cc8e8512ff3555bd64d2
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-24 04:00:04 -0700

    fix: use new binary cache key

--- hosts/common/optional/distributed-builds.nix
+++ hosts/common/optional/distributed-builds.nix
@@ -8,15 +8,13 @@
       {
         hostName = "nzxt";
         system = "x86_64-linux";
-        # if the builder supports building for multiple architectures,
-        # replace the previous line by, e.g.,
-        # systems = ["x86_64-linux" "aarch64-linux"];
         maxJobs = 8;
         speedFactor = 2;
         supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
         mandatoryFeatures = [];
       }
     ];
+    settings.max-jobs = 0;
     distributedBuilds = true;
     # optional, useful when the builder has a faster internet connection than yours
     extraOptions = ''
