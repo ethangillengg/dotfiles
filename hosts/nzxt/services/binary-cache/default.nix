2a2ea911e8c342e965497fa506d414df29db15b6
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-24 04:00:04 -0700

    fix: use new binary cache key
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/nzxt/services/binary-cache/default.nix
+++ hosts/nzxt/services/binary-cache/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   port,
   ...
 }: {
@@ -11,8 +10,6 @@
     nix-serve = {
       enable = true;
       secretKeyFile = config.sops.secrets.cache-sig-key.path;
-      # TODO: temporary fix for NixOS/nix#7704
-      package = pkgs.nix-serve.override {nix = pkgs.nixVersions.nix_2_12;};
       inherit port;
     };
   };
