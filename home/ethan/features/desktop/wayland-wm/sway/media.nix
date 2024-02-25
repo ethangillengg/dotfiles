36a604485e8d90aaf68e6dfabc64c337f632969b
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-24 19:25:15 -0700

    fix(swayosd): working again!
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- home/ethan/features/desktop/wayland-wm/sway/media.nix
+++ home/ethan/features/desktop/wayland-wm/sway/media.nix
@@ -1,33 +1,29 @@
 {pkgs, ...}: let
-  # swayosd = "${pkgs.swayosd}/bin/swayosd";
-  light = "${pkgs.light}/bin/light";
-  pamixer = "${pkgs.pamixer}/bin/pamixer";
+  swayosd-client = "${pkgs.swayosd}/bin/swayosd-client";
 in {
   # On-screen notifications for media controls (eg. brightness, volume, etc.)
   services.swayosd = {
     enable = true;
   };
-  # maxVolume = 125;
-  wayland.windowManager.sway.config.keybindings = {
-    # TODO: Wait for upstream fix of swayosd-libinput-backend service for NixOS
-    ## Media Controls
-    # "XF86MonBrightnessUp" = "exec ${swayosd} --brightness +5";
-    # "XF86MonBrightnessDown" = "exec ${swayosd} --brightness -5";
+  home.packages = with pkgs; [
+    swayosd
+  ];
 
-    # "XF86AudioRaiseVolume" = "exec ${swayosd} --output-volume +5";
-    # "XF86AudioLowerVolume" = "exec ${swayosd} --output-volume -5";
-    # "XF86AudioMute" = "exec ${swayosd} --output-volume mute-toggle";
-    # "XF86AudioMicMute" = "exec ${swayosd} --input-volume mute-toggle";
+  wayland.windowManager.sway = {
+    config.keybindings = {
+      ## Brightness
+      "XF86MonBrightnessUp" = "exec ${swayosd-client} --brightness +5";
+      "XF86MonBrightnessDown" = "exec ${swayosd-client} --brightness -5";
 
-    ## Notify Pressed
-    # "Caps_Lock" = "exec ${swayosd} --caps-lock";
-
-    "XF86AudioRaiseVolume" = "exec ${pamixer} -i 5";
-    "XF86AudioLowerVolume" = "exec ${pamixer} -d 5";
-    "XF86AudioMute" = "exec ${pamixer} -t";
-    "XF86AudioMicMute" = "exec ${pamixer} --default-source -t";
-
-    "XF86MonBrightnessUp" = "exec ${light} -A 5%";
-    "XF86MonBrightnessDown" = "exec ${light} -U 5%";
+      ## Volume
+      "XF86AudioRaiseVolume" = "exec ${swayosd-client} --output-volume +5";
+      "XF86AudioLowerVolume" = "exec ${swayosd-client} --output-volume -5";
+      "XF86AudioMute" = "exec ${swayosd-client} --output-volume mute-toggle";
+      "XF86AudioMicMute" = "exec ${swayosd-client} --input-volume mute-toggle";
+    };
+    ## Caps Lock
+    extraConfig = ''
+      bindsym --release Caps_Lock exec ${swayosd-client} --caps-lock
+    '';
   };
 }
