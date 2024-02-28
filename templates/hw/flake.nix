30d4f9eb9f0681362e6a60ea1e996b3cf2cc6b9f
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-28 09:31:50 -0700

    feat(hw): tectonic template

--- templates/hw/flake.nix
+++ templates/hw/flake.nix
@@ -13,65 +13,17 @@
         system = "x86_64-linux"; # or something else
         config = {allowUnfree = true;};
       };
-      pname = "hw";
-      name = pname;
 
       watcher = pkgs.writeScriptBin "watch" ''
-        out=".latexmkout"
-        mkdir "$out"
-        latexmk \
-          -pvc \
-          -outdir="$out" \
-          -pdf \
-          -time \
-          -quiet \
-          -use-make ${pname}.tex
-        ${pname}.tex
-        rm -r "$out"
+        tectonic -X build
+        nohup sioyek build/hw/hw.pdf >/dev/null 2>&1& tectonic -X watch
       '';
-
-      buildLatex = pkgs.stdenv.mkDerivation {
-        inherit pname;
-
-        src = ./.;
-
-        nativeBuildInputs = with pkgs; [
-          (texlive.combine {
-            inherit
-              (texlive)
-              multirow
-              hyperref
-              blindtext
-              fancyhdr
-              etoolbox
-              topiclongtable
-              ;
-          })
-          gnumake
-        ];
-
-        buildPhase = ''
-          latexmk \
-          -pdf \
-          -pdflatex="pdflatex -interaction=nonstopmode" \
-          -use-make ${pname}.tex
-        '';
-        installPhase = ''
-          install -Dm444 -t $out ${pname}.pdf
-        '';
-      };
     in {
-      packages.${pname} = pkgs.stdenv.mkDerivation {
-        inherit watcher buildLatex name;
-      };
-
-      # defaultPackage = packages.virtual-orrery;
       devShell = pkgs.mkShell {
         nativeBuildInputs = [pkgs.bashInteractive];
         buildInputs = with pkgs; [
-          rubber
           texlab
-          texliveFull
+          tectonic
         ];
       };
 
