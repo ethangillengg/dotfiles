3173a9182a08f63759584c06293a000b6aefb137
Ethan Gill <ethan.gill@ucalgary.ca>
2024-02-29 17:56:21 -0700

    feat: yazi

--- home/ethan/features/cli/yazi.nix
+++ home/ethan/features/cli/yazi.nix
@@ -1,12 +1,17 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  config,
+  ...
+}: let
   # Dependencies
   nvim = "${pkgs.neovim}/bin/nvim";
   #unpack .zip, .tar, .rar with one command
-  unar = "${pkgs.unar}/bin/unar";
+  unar = "${pkgs.ouch}/bin/ouch";
 in {
   programs.yazi = {
     enable = true;
     settings = {
+      manager.linemode = "permissions";
       opener = {
         "archive" = [
           {
@@ -21,112 +26,10 @@ in {
         ];
       };
     };
-
-    theme = {
-      # icons = {
-      #   "Desktop/" = "";
-      #   "Documents/" = "";
-      #   "Downloads/" = "";
-      #   "Pictures/" = "";
-      #   "Music/" = "";
-      #   "Movies/" = "";
-      #   "Videos/" = "";
-      #   "Public/" = "";
-      #   "Library/" = "";
-      #   "Code/" = "";
-      #   ".config/" = "";
-      #
-      #   # Git;
-      #   ".git/" = "";
-      #   ".gitignore" = "";
-      #   ".gitmodules" = "";
-      #   ".gitattributes" = "";
-      #
-      #   # Dotfiles;
-      #   ".DS_Store" = "";
-      #   ".bashrc" = "";
-      #   ".bashprofile" = "";
-      #   ".zshrc" = "";
-      #   ".zshenv" = "";
-      #   ".zprofile" = "";
-      #   ".vimrc" = "";
-      #
-      #   # Text;
-      #   "*.txt" = "";
-      #   "*.md" = "";
-      #
-      #   # Archives;
-      #   "*.zip" = "";
-      #   "*.tar" = "";
-      #   "*.gz" = "";
-      #   "*.7z" = "";
-      #
-      #   # Audio;
-      #   "*.mp3" = "";
-      #   "*.flac" = "";
-      #   "*.wav" = "";
-      #
-      #   # Movies;
-      #   "*.mp4" = "";
-      #   "*.mkv" = "";
-      #   "*.avi" = "";
-      #   "*.mov" = "";
-      #
-      #   # Images;
-      #   "*.jpg" = "";
-      #   "*.jpeg" = "";
-      #   "*.png" = "";
-      #   "*.gif" = "";
-      #   "*.webp" = "";
-      #   "*.avif" = "";
-      #   "*.bmp" = "";
-      #   "*.ico" = "";
-      #   "*.svg" = "";
-      #
-      #   # Custom
-      #   "*.nix" = "";
-      #   "*.key" = "󰌆";
-      #   "*.pdf" = "";
-      #
-      #   # Programming;
-      #   "*.c" = "";
-      #   "*.cpp" = "";
-      #   "*.h" = "";
-      #   "*.hpp" = "";
-      #   "*.rs" = "";
-      #   "*.go" = "";
-      #   "*.py" = "";
-      #   "*.js" = "";
-      #   "*.ts" = "";
-      #   "*.tsx" = "";
-      #   "*.jsx" = "";
-      #   "*.rb" = "";
-      #   "*.php" = "";
-      #   "*.java" = "";
-      #   "*.sh" = "";
-      #   "*.fish" = "";
-      #   "*.swift" = "";
-      #   "*.vim" = "";
-      #   "*.lua" = "";
-      #   "*.html" = "";
-      #   "*.css" = "";
-      #   "*.scss" = "";
-      #   "*.json" = "";
-      #   "*.toml" = "";
-      #   "*.yml" = "";
-      #   "*.yaml" = "";
-      #   "*.ini" = "";
-      #   "*.conf" = "";
-      #
-      #   # Default;
-      #   "*" = "";
-      #   "*/" = "";
-      # };
-    };
   };
 
   home.shellAliases = {
     # "lf" = "yazi";
-    # "y" = "yazi";
+    "y" = "yazi";
   };
 }
