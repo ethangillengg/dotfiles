8c2d1a2ab2a973870f51a329f63e27834addb22e
Ethan Gill <ethan.gill@ucalgary.ca>
2024-03-20 10:37:24 -0600

    feat(pass-tofi): better sh syntax

--- pkgs/pass-tofi/pass-tofi.sh
+++ pkgs/pass-tofi/pass-tofi.sh
@@ -1,27 +1,11 @@
 cd "${PASSWORD_STORE_DIR:-$HOME/.password-store}"
 
-if [ "$XDG_CURRENT_DESKTOP" == "Hyprland" ]; then
-    class="$(hyprctl activewindow -j | jq -r '.class')"
-elif [ "$XDG_CURRENT_DESKTOP" == "sway" ]; then
-    focused="$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused==true)')"
-    class=$(jq -r '.window_properties.class' <<< "$focused")
-fi
-
-if [[ "$class" == "org.qutebrowser.qutebrowser" ]]; then
-    qutebrowser :yank
-    query=$(wl-paste | cut -d '/' -f3 | sed s/"www."//)
-elif [[ "$class" == "discord" ]]; then
-    query="discord.com"
-elif [[ "$class" == "Steam" ]]; then
-    query="steampowered.com"
-fi
-
-selected=$(find -L . -not -path '*\/.*' -path "*.gpg" -type f -printf '%P\n' | \
-  sed 's/.gpg$//g' | \
-  tofi --prompt-text "Select Password: " ) || exit 2
+selected=$(find -L . -not -path '*\/.*' -path "*.gpg" -type f -printf '%P\n' |
+	sed 's/.gpg$//g' |
+	tofi --prompt-text "Select Password: ") || exit 2
 
 if [ -z "$selected" ]; then
-    exit 1
+	exit 1
 fi
 
 username=$(echo "$selected" | cut -d '/' -f2)
@@ -35,29 +19,41 @@ URL"
 field=$(printf "$fields" | tofi --prompt-text "Select Field: ") || field="Password"
 
 case "$field" in
-    "Password")
-        value="$(pass "$selected" | head -n 1)" && [ -n "$value" ] || \
-            { notify-send "Error" "No password for $selected" -i error -t 6000; exit 3; }
-        ;;
-    "Username")
-        value="$username"
-        ;;
-    "URL")
-        value="$url"
-        ;;
-    "OTP")
-        value="$(pass otp "$selected")" || \
-            { notify-send "Error" "No OTP for $selected" -i error -t 6000; exit 3; }
-        ;;
-    *)
-        exit 4
+"Password")
+	value="$(pass "$selected" | head -n 1)"
+	if [ -z "$value" ]; then
+		{
+			notify-send "Error" "No password for $selected" -i error -t 6000
+			exit 3
+		}
+	fi
+	;;
+"Username")
+	value="$username"
+	;;
+"URL")
+	value="$url"
+	;;
+"OTP")
+	value="$(pass otp "$selected")"
+	if [ -z "$value" ]; then
+		{
+			notify-send "Error" "No OTP for $selected" -i error -t 6000
+			exit 3
+		}
+	fi
+	;;
+*)
+	exit 4
+	;;
 esac
 
 wl-copy "$value"
 case "$field" in
-    "Password")
-            notify-send "Password Copied!" -i edit-copy -t 4000
-        ;;
-      *)
-            notify-send "Copied $field:" "$value" -i edit-copy -t 4000
+"Password")
+	notify-send "Password Copied!" -i edit-copy -t 4000
+	;;
+*)
+	notify-send "Copied $field:" "$value" -i edit-copy -t 4000
+	;;
 esac
