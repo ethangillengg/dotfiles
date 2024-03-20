dc56256714ec90d2fd1e1428edc4490167f0118d
Ethan Gill <ethan.gill@ucalgary.ca>
2024-03-20 10:38:04 -0600

    feat(nvim): luarock for image.nvim
    Signed-off-by: Ethan Gill <ethan.gill@ucalgary.ca>

--- hosts/common/nvim.nix
+++ hosts/common/nvim.nix
@@ -1,7 +1,27 @@
-# This file (and the global directory) holds config that i use on all hosts
-{pkgs, ...}: {
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/neovim/utils.nix#L27
+{pkgs, ...}: let
+  config = pkgs.neovimUtils.makeNeovimConfig {
+    extraLuaPackages = p: [p.luarocks p.magick];
+    withNodeJs = false;
+    withRuby = false;
+    withPython3 = false;
+    # https://github.com/NixOS/nixpkgs/issues/211998
+    customRC = "luafile ~/.config/nvim/init.lua";
+  };
+in {
+  nixpkgs.overlays = [
+    (_: super: {
+      neovim-custom =
+        pkgs.wrapNeovimUnstable
+        (super.neovim-unwrapped.overrideAttrs (oldAttrs: {
+          version = "master";
+          buildInputs = oldAttrs.buildInputs ++ [super.tree-sitter];
+        }))
+        config;
+    })
+  ];
   environment.systemPackages = with pkgs; [
-    neovim
+    neovim-custom
 
     # Nix
     nil
@@ -24,5 +44,7 @@
     gopls
     go
     deno
+    nodePackages_latest.volar
+    nodePackages_latest.typescript-language-server
   ];
 }
