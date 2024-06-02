ae76cb3395b2e6c27d90559a62f10faf122afad9
Ethan Gill <ethan.gill@ucalgary.ca>
2024-06-02 16:33:59 -0600

    chore(nzxt): roll key for binary cache

--- hosts/nzxt/services/binary-cache/default.nix
+++ hosts/nzxt/services/binary-cache/default.nix
@@ -3,13 +3,13 @@
   port,
   ...
 }: {
-  sops.secrets.cache-sig-key = {
+  sops.secrets.binary-cache-priv-key = {
     sopsFile = ../../secrets.yaml;
   };
   services = {
     nix-serve = {
       enable = true;
-      secretKeyFile = config.sops.secrets.cache-sig-key.path;
+      secretKeyFile = config.sops.secrets.binary-cache-priv-key.path;
       inherit port;
     };
   };
