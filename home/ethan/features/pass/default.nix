df80df3c21465a5a43b0e0f2e594d5b4cada6177
Ethan Gill <ethan.gill@ucalgary.ca>
2024-08-02 14:56:17 -0600

    feat: auto-sync password store

--- home/ethan/features/pass/default.nix
+++ home/ethan/features/pass/default.nix
@@ -1,10 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.password-store = {
     enable = true;
-    settings = {PASSWORD_STORE_DIR = "$HOME/.password-store";};
+    settings = {PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";};
     package = pkgs.pass.withExtensions (p: [p.pass-otp]);
   };
 
   # firefox extension
   programs.browserpass.enable = true;
+
+  # auto sync passwords using git
+  services.git-sync = {
+    enable = true;
+    repositories = {
+      passwords = {
+        path = "${config.home.homeDirectory}/.password-store";
+        uri = "git@github.com:ethangillengg/passwords.git";
+      };
+    };
+  };
 }
