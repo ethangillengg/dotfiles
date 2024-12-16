76f5589a520d3afab4e9ffe890b25e28d38977e8
Ethan Gill <ethan.gill@ucalgary.ca>
2024-12-15 22:31:25 -0700

    fix: sonarr new dotnet

--- hosts/nzxt/services/servarr/sonarr.nix
+++ hosts/nzxt/services/servarr/sonarr.nix
@@ -4,6 +4,15 @@
   config,
   ...
 }: {
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-wrapped-6.0.428"
+    "dotnet-sdk-6.0.428"
+    "dotnet-core-combined"
+    "aspnetcore-runtime-6.0.36"
+    "dotnet-sdk-7.0.410"
+  ];
+
   services.sonarr = {
     enable = true;
     openFirewall = true;
