25ff938045f2456f6be2ce0d0efcf25c5c7cd52b
Ethan Gill <gill.ethan123@gmail.com>
2025-03-02 15:56:43 -0700

    feat: iommu dual gpu

--- hosts/common/optional/qemu.nix
+++ hosts/common/optional/qemu.nix
@@ -7,4 +7,19 @@
   ];
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  users.groups.libvirtd.members = ["ethan"];
+
+  boot = {
+    kernelParams = ["amd_iommu=on"];
+    initrd.kernelModules = [
+      "vfio_pci"
+      "vfio"
+      "vfio_iommu_type1"
+    ];
+    extraModprobeConfig = ''
+      softdep nvidia pre: vfio-pci
+      options vfio-pci ids=10de:1b80,10de:10f0
+    '';
+  };
 }
