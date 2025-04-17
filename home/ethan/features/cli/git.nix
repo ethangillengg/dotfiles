d0995af79f16c7db6c6297ad29012dee7670f37e
Ethan Gill <gill.ethan123@gmail.com>
2025-04-17 09:09:32 -0600

    feat(git): use ssh-agent instead of gpg-agent for ssh

--- home/ethan/features/cli/git.nix
+++ home/ethan/features/cli/git.nix
@@ -1,4 +1,5 @@
 {
+  services.ssh-agent.enable = true;
   programs.git = {
     enable = true;
     userName = "Ethan Gill";
--- home/ethan/features/cli/gpg/default.nix
+++ home/ethan/features/cli/gpg/default.nix
@@ -8,7 +8,6 @@ in {
   home.file.".gnupg/gpg.conf".source = ./gpg.conf;
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
     defaultCacheTtl = minsToSecs 30;
     enableExtraSocket = true;
     inherit pinentryPackage;
