dd39137e2fe1645fbdfb930dd5d11be9a8288285
Ethan Gill <gill.ethan123@gmail.com>
2025-06-19 20:46:02 -0600

    chore: update deprecated options 2

--- home/ethan/features/desktop/wayland-wm/mako.nix
+++ home/ethan/features/desktop/wayland-wm/mako.nix
@@ -3,24 +3,26 @@
 in {
   services.mako = {
     enable = true;
-    iconPath =
-      if variant == "dark"
-      then "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
-      else "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    font = "${config.fontProfiles.regular.family} 12";
-    padding = "10,15";
-    anchor = "top-right";
-    width = 450;
-    height = 200;
-    borderSize = 2;
-    defaultTimeout = 8000;
-    groupBy = "app-name";
-    backgroundColor = "#${palette.base00}dd";
-    borderColor = "#${palette.base03}dd";
-    textColor = "#${palette.base05}dd";
-    extraConfig = ''
-      [mode=do-not-disturb]
-      invisible=1
-    '';
+    settings = {
+      iconPath =
+        if variant == "dark"
+        then "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
+        else "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
+      font = "${config.fontProfiles.regular.family} 12";
+      padding = "10,15";
+      anchor = "top-right";
+      width = 450;
+      height = 200;
+      borderSize = 2;
+      defaultTimeout = 8000;
+      groupBy = "app-name";
+      backgroundColor = "#${palette.base00}dd";
+      borderColor = "#${palette.base03}dd";
+      textColor = "#${palette.base05}dd";
+      extraConfig = ''
+        [mode=do-not-disturb]
+        invisible=1
+      '';
+    };
   };
 }
