d754ca08cc1aa0b062408c84a34a39945539a309
Ethan Gill <gill.ethan123@gmail.com>
2025-12-01 13:10:55 -0700

    feat(zellij): config

--- home/ethan/features/cli/zellij/default.nix
+++ home/ethan/features/cli/zellij/default.nix
@@ -1,16 +1,11 @@
-{
+{lib, ...}: {
   xdg.configFile."zellij/config.kdl" = {
     source = ./config.kdl;
   };
 
+  # home.sessionVariables.ZELLIJ_AUTO_ATTACH = lib.mkForce "false";
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      pane_frames = false;
-      mouse_mode = false;
-      theme = "gruvbox-dark";
-      default_mode = "locked";
-    };
   };
 }
